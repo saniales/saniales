456ebea1202cb8fabb2421a066dc5464fcb5f9c3
Alessandro Sanino <alessandro@sanino.dev>
2020-08-30 20:37:51 +0200

    Added preferred stack

--- index.js
+++ index.js
@@ -18,6 +18,172 @@ let DATA = {
     timeZoneName: 'short',
     timeZone: 'Europe/Rome',
   }),
+  preferred_stack: [
+    {
+      group: "IDE",
+      items: [
+        {
+          name: "Visual Studio Code",
+          encoded_name: "Visual%20Studio%20Code",
+          color: "purple",
+          logo: "visual-studio-code",
+          logo_color: "white"
+        }
+      ]
+    },
+    {
+      group: "Cloud",
+      items: [
+        {
+          name: "Docker",
+          encoded_name: "Docker",
+          color: "blue",
+          logo: "docker",
+          logo_color: "white"
+        },
+        {
+          name: "Heroku",
+          encoded_name: "Heroku",
+          color: "purple",
+          logo: "heroku",
+          logo_color: "white"
+        },
+        {
+          name: "Rancher Certified",
+          encoded_name: "Rancher%20Certified",
+          color: "blue",
+          logo: "rancher",
+          logo_color: "white"
+        }
+      ]
+    },
+    {
+      group: "DevOps",
+      items: [
+        {
+          name: "Gitlab Pipelines",
+          encoded_name: "Gitlab%20Pipelines",
+          color: "orange",
+          logo: "gitlab",
+          logo_color: "white"
+        },
+        {
+          name: "Github Actions",
+          encoded_name: "Github%20Actions",
+          color: "black",
+          logo: "github",
+          logo_color: "white"
+        }
+      ]
+    },
+    {
+      group: "CDN",
+      items: [
+        {
+          name: "Cloudflare",
+          encoded_name: "Cloudflare",
+          color: "yellow",
+          logo: "gitlab",
+          logo_color: "orange"
+        }
+      ]
+    },
+    {
+      group: "Blockchain",
+      items: [
+        {
+          name: "Solidity",
+          encoded_name: "Solidity",
+          color: "black",
+          logo: "ethereum",
+          logo_color: "green"
+        },
+        {
+          name: "Truffle",
+          encoded_name: "Truffle",
+          color: "black",
+          logo: "ethereum",
+          logo_color: "green"
+        },
+        {
+          name: "Ganache",
+          encoded_name: "Ganache",
+          color: "black",
+          logo: "ethereum",
+          logo_color: "green"
+        }
+      ]
+    },
+    {
+      group: "Backend",
+      items: [
+        {
+          name: "Golang",
+          encoded_name: "Golang",
+          color: "blue",
+          logo: "go",
+          logo_color: "white"
+        },
+        {
+          name: "Goa Design framework (also maintainer)",
+          encoded_name: "Goa%20Design%20framework%20(also%20maintainer)",
+          color: "blue",
+          logo: "go",
+          logo_color: "white"
+        },
+        {
+          name: "Redis",
+          encoded_name: "Redis",
+          color: "red",
+          logo: "redis",
+          logo_color: "white"
+        },
+        {
+          name: "MongoDB & Atlas",
+          encoded_name: "MongoDB%20&%20Atlas",
+          color: "green",
+          logo: "mongodb",
+          logo_color: "white"
+        }
+      ]
+    },
+    {
+      group: "Frontend",
+      items: [
+        {
+          name: "VueJS",
+          encoded_name: "VueJS",
+          color: "green",
+          logo: "vue.js",
+          logo_color: "white"
+        },
+        {
+          name: "Nuxt",
+          encoded_name: "Nuxt",
+          color: "green",
+          logo: "nuxt",
+          logo_color: "white"
+        },
+        {
+          name: "Realm DB",
+          encoded_name: "Realm DB",
+          color: "purple",
+          logo: "realm",
+          logo_color: "white"
+        },
+        {
+          name: "Typescript",
+          encoded_name: "Typescript",
+          color: "blue",
+          logo: "typescript",
+          logo_color: "white"
+        }
+      ]
+    }
+  ],
+  projects: {
+
+  }
 };
 
 /**
@@ -26,7 +192,7 @@ let DATA = {
   * C - We create a README.md file with the generated output
   */
 function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
